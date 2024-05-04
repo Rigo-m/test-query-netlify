fe4085f7321de2701268f841759d5a3a043f55bc
Matteo Rigoni <matteo.rigoni@atoms.studio>
2024-05-04 15:31:55 +0200

    noqs

--- app/router.options.ts
+++
@@ -1,8 +0,0 @@
-import type { RouterConfig } from "@nuxt/schema";
-import qs from "qs";
-
-// https://router.vuejs.org/api/interfaces/routeroptions.html
-export default <RouterConfig>{
-  parseQuery: qs.parse,
-  stringifyQuery: qs.stringify,
-};
